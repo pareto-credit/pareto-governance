f93291ed671cc9eea091f604fb7251024ee65e79
bugduino <william@idle.finance>
2025-09-11 18:47:59 +0200

    FIX: add natspec comments

--- src/Pareto.sol
+++ src/Pareto.sol
@@ -6,26 +6,40 @@ import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20P
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
 
+/// @title Pareto (PAR) ERC20 token
+/// @notice Governance token of the Pareto ecosystem
+/// @dev Token is timestamp-based for voting snapshots (see IERC6372)
 contract Pareto is ERC20, ERC20Permit, ERC20Votes {
+  /// @notice Initial supply of 18.2 million PAR
   constructor() ERC20("Pareto", "PAR") ERC20Permit("Pareto") {
     _mint(msg.sender, 18_200_000 * 10**18);
   }
 
-  // Overrides IERC6372 functions to make the token & governor timestamp-based
+  /// @notice Current clock time used for voting snapshots
+  /// @dev Overrides IERC6372 functions to make the token & governor timestamp-based
   function clock() public view override returns (uint48) {
     return uint48(block.timestamp);
   }
 
+  /// @notice Clock mode used for voting snapshots
+  /// @return string describing the clock mode
   // solhint-disable-next-line func-name-mixedcase
   function CLOCK_MODE() public pure override returns (string memory) {
     return "mode=timestamp";
   }
 
-  // The functions below are overrides required by Solidity.
+  /// @notice See {ERC20-_update}.
+  /// @dev The functions below are overrides required by Solidity.
+  /// @param from address tokens are moved from
+  /// @param to address tokens are moved to
+  /// @param amount of tokens moved
   function _update(address from, address to, uint256 amount) internal override(ERC20, ERC20Votes) {
     super._update(from, to, amount);
   }
 
+  /// @notice See {IERC20Permit-nonces}.
+  /// @param owner address to query
+  /// @return the current nonce for `owner`
   function nonces(address owner) public view virtual override(ERC20Permit, Nonces) returns (uint256) {
     return super.nonces(owner);
   }
--- src/ParetoGovernor.sol
+++ src/ParetoGovernor.sol
@@ -10,6 +10,9 @@ import {TimelockController} from "@openzeppelin/contracts/governance/TimelockCon
 import {IVotes} from "@openzeppelin/contracts/governance/utils/IVotes.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+/// @title ParetoGovernor
+/// @notice Governor contract for the Pareto ecosystem
+/// @dev Uses a timelock for executing successful proposals
 contract ParetoGovernor is
   Governor,
   GovernorCountingSimple,
@@ -17,35 +20,57 @@ contract ParetoGovernor is
   GovernorVotesQuorumFraction,
   GovernorTimelockControl
 {
+  /// @notice Creates a new ParetoGovernor
+  /// @param _token address of the governance token (must implement IVotes)
+  /// @param _timelock address of the TimelockController to be used
   constructor(
     IVotes _token,
     TimelockController _timelock
   ) Governor("ParetoGovernor") GovernorVotes(_token) GovernorVotesQuorumFraction(4) GovernorTimelockControl(_timelock) {}
 
+  /// @notice Voting delay in seconds
+  /// @return delay in seconds
   function votingDelay() public pure override returns (uint256) {
     return 10 minutes;
   }
 
+  /// @notice Voting period in seconds
+  /// @return period in seconds
   function votingPeriod() public pure override returns (uint256) {
     return 3 days;
   }
 
+  /// @notice Minimum number of tokens required to propose
+  /// @return number of tokens required
   function proposalThreshold() public view override returns (uint256) {
     return IERC20(address(token())).totalSupply() * 1 / 100; // 1% of token supply
   }
 
-  // The functions below are overrides required by Solidity.
-
+  /// @notice State of a proposal
+  /// @dev This override is required by Solidity.
+  /// @param proposalId id of the proposal to query
+  /// @return state of the proposal
   function state(uint256 proposalId) public view override(Governor, GovernorTimelockControl) returns (ProposalState) {
     return super.state(proposalId);
   }
 
+  /// @notice Whether a proposal needs to be queued in the timelock
+  /// @dev This override is required by Solidity.
+  /// @param proposalId id of the proposal to query
+  /// @return true if the proposal needs to be queued, false otherwise
   function proposalNeedsQueuing(
     uint256 proposalId
   ) public view virtual override(Governor, GovernorTimelockControl) returns (bool) {
     return super.proposalNeedsQueuing(proposalId);
   }
 
+  /// @notice Internal function to queue operations in the timelock
+  /// @dev This override is required by Solidity.
+  /// @param proposalId id of the proposal to queue
+  /// @param targets list of target addresses for calls to be made
+  /// @param values list of values (in wei) to be sent with each call
+  /// @param calldatas list of calldata for each call
+  /// @param descriptionHash hash of the proposal description
   function _queueOperations(
     uint256 proposalId,
     address[] memory targets,
@@ -56,6 +81,13 @@ contract ParetoGovernor is
     return super._queueOperations(proposalId, targets, values, calldatas, descriptionHash);
   }
 
+  /// @notice Internal function to execute operations in the timelock
+  /// @dev This override is required by Solidity.
+  /// @param proposalId id of the proposal to execute
+  /// @param targets list of target addresses for calls to be made
+  /// @param values list of values (in wei) to be sent with each call
+  /// @param calldatas list of calldata for each call
+  /// @param descriptionHash hash of the proposal description
   function _executeOperations(
     uint256 proposalId,
     address[] memory targets,
@@ -66,6 +98,13 @@ contract ParetoGovernor is
     super._executeOperations(proposalId, targets, values, calldatas, descriptionHash);
   }
 
+  /// @notice Internal function to cancel a proposal
+  /// @dev This override is required by Solidity.
+  /// @param targets list of target addresses for calls to be made
+  /// @param values list of values (in wei) to be sent with each call
+  /// @param calldatas list of calldata for each call
+  /// @param descriptionHash hash of the proposal description
+  /// @return id of the canceled proposal
   function _cancel(
     address[] memory targets,
     uint256[] memory values,
@@ -75,6 +114,9 @@ contract ParetoGovernor is
     return super._cancel(targets, values, calldatas, descriptionHash);
   }
 
+  /// @notice Executor address
+  /// @dev This override is required by Solidity.
+  /// @return address of the executor
   function _executor() internal view override(Governor, GovernorTimelockControl) returns (address) {
     return super._executor();
   }
